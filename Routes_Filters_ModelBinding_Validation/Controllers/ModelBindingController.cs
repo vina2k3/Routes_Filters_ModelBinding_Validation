768308f13f6f157e940b3e4651d73df26525e053
Vinicius de Souza <vsouza@usf.edu>
2020-04-10 16:16:06 -0400

    Vini

--- Routes_Filters_ModelBinding_Validation/Controllers/ModelBindingController.cs
+++ Routes_Filters_ModelBinding_Validation/Controllers/ModelBindingController.cs
@@ -7,35 +7,50 @@ using Routes_Filters_ModelBinding_Validation.Models;
 
 namespace Routes_Filters_ModelBinding_Validation.Controllers
 {
-  public class ModelBindingController : Controller
-  {
-    private IRepository repository;
-
-    public ModelBindingController(IRepository repo)
+    public class ModelBindingController : Controller
     {
-      repository = repo;
+        private IRepository repository;
+
+        public ModelBindingController(IRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(int id) => View(repository[id]);
+
+        public ViewResult IndexWithAddress(int id) => View(repository[id]);
+
+        public ViewResult IndexNullSafe(int id) => View(repository[id] ?? repository.People.First());
+
+        public ViewResult Create() => View(new Person());
+
+        [HttpPost]
+        public ViewResult Create(Person model) => View("IndexNullSafe", model);
+
+        public ViewResult CreateWithAddress() => View(new Person());
+
+        [HttpPost]
+        public ViewResult CreateWithAddress(Person model) => View("IndexWithAddress", model);
+
+        public ViewResult NamesArray(string[] names) => View(names ?? new string[0]);
+
+        public ViewResult NamesList(IList<string> names) => View(names ?? new List<string>());
+
+        public ViewResult Address(IList<AddressSummary> addresses) => View(addresses ?? new List<AddressSummary>());
+
+        public ViewResult createVehicle()
+        {
+
+            Vehicle vehicle = new Vehicle();
+            return View(vehicle);
+        }
+
+        [HttpPost]
+
+        public ViewResult createVehicle(Vehicle vehicle)
+        {
+            return View(vehicle);
+        }
+
     }
-
-    public ViewResult Index(int id) => View(repository[id]);
-
-    public ViewResult IndexWithAddress(int id) => View(repository[id]);
-
-    public ViewResult IndexNullSafe(int id) => View(repository[id] ?? repository.People.First());
-
-    public ViewResult Create() => View(new Person());
-
-    [HttpPost]
-    public ViewResult Create(Person model) => View("IndexNullSafe", model);
-
-    public ViewResult CreateWithAddress() => View(new Person());
-
-    [HttpPost]
-    public ViewResult CreateWithAddress(Person model) => View("IndexWithAddress", model);
-
-    public ViewResult NamesArray(string[] names) => View(names ?? new string[0]);
-
-    public ViewResult NamesList(IList<string> names) => View(names ?? new List<string>());
-
-    public ViewResult Address(IList<AddressSummary> addresses) => View(addresses ?? new List<AddressSummary>());
-  }
 }
